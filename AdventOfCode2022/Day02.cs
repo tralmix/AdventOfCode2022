7ad7aa982732c36075a61246187c15c6c736779c
tralmix <alex.uhling@gmail.com>
2022-12-03 18:07:15 -0500

    Completed part 2

--- AdventOfCode2022/Day02.cs
+++ AdventOfCode2022/Day02.cs
@@ -10,8 +10,8 @@
             var part1Answer = await Part1(FileName);
             Console.WriteLine($"The score for all rounds is {part1Answer}.");
 
-            //var part2Answer = await Part2(FileName);
-            //Console.WriteLine($"The three elves carrying the most hold a combined {part2Answer} calories");
+            var part2Answer = await Part2(FileName);
+            Console.WriteLine($"The score for all rounds is {part2Answer}.");
 
             Console.WriteLine();
         }
@@ -23,6 +23,13 @@
             return rounds.Sum(r => r.Score);
         }
 
+        public static async Task<int> Part2(string fileName)
+        {
+            var rounds = await ParseFile(fileName, false);
+
+            return rounds.Sum(r => r.Score);
+        }
+
         public static async Task<List<Round>> ParseFile(string fileName, bool part1 = true)
         {
             var linesOfInput = await File.ReadAllLinesAsync(fileName);
@@ -30,36 +37,71 @@
             var rounds = new List<Round>();
 
             foreach (var line in linesOfInput)
-                if (part1) rounds.Add(BuildRound_Part1(line));
-                else
-                {
-
-                }
+                if (part1) rounds.Add(BuildRound(line, part1));
+                else rounds.Add(BuildRound(line, part1));
 
             return rounds;
         }
 
-        private static Round BuildRound_Part1(string line)
+        private static Round BuildRound(string line, bool part1)
         {
             var throws = line.Split(' ');
             var round = new Round
             {
-                Opponent = throws[0] switch
-                {
-                    "A" => Throw.Rock,
-                    "B" => Throw.Paper,
-                    "C" => Throw.Scissors,
-                    _ => throw new NotImplementedException()
-                },
-                Self = throws[1] switch
-                {
-                    "X" => Throw.Rock,
-                    "Y" => Throw.Paper,
-                    "Z" => Throw.Scissors,
-                    _ => throw new NotImplementedException()
-                }
+                Opponent = GetThrowValue(throws[0]),
+                Self = part1 ? GetThrowValue(throws[1]) :
+                    GetThrowValueBasedOnOpponentAndExpectedOutcome(throws)
             };
             return round;
+        }
+
+        private static Throw GetThrowValueBasedOnOpponentAndExpectedOutcome(string[] throws)
+        {
+            return throws[1] switch
+            {
+                "X" => GetThrowToLose(GetThrowValue(throws[0])),
+                "Y" => GetThrowToDraw(GetThrowValue(throws[0])),
+                "Z" => GetThrowToWin(GetThrowValue(throws[0])),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        private static Throw GetThrowToLose(Throw opponentThrow)
+        {
+            return opponentThrow switch
+            {
+                Throw.Rock => Throw.Scissors,
+                Throw.Paper => Throw.Rock,
+                Throw.Scissors => Throw.Paper,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        private static Throw GetThrowToWin(Throw opponentThrow)
+        {
+            return opponentThrow switch
+            {
+                Throw.Rock => Throw.Paper,
+                Throw.Paper => Throw.Scissors,
+                Throw.Scissors => Throw.Rock,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        private static Throw GetThrowToDraw(Throw opponentThrow) => opponentThrow;
+
+        private static Throw GetThrowValue(string input)
+        {
+            return input switch
+            {
+                "A" => Throw.Rock,
+                "B" => Throw.Paper,
+                "C" => Throw.Scissors,
+                "X" => Throw.Rock,
+                "Y" => Throw.Paper,
+                "Z" => Throw.Scissors,
+                _ => throw new NotImplementedException()
+            };
         }
     }
 
