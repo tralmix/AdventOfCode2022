e6b140455a181589bed62f7d588acd3e080ae844
tralmix <alex.uhling@gmail.com>
2022-12-24 16:03:34 -0500

    Day 6 completed

---
+++ AdventOfCode2022.Tests/Day06/FindMarker.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode2022.Day_06
+{
+    public class FindMarker
+    {
+        [Theory]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        public void FindSignalMarker_ShouldReturnExpectedMarker(string input, int expected)
+        {
+            var marker = Day06.FindSignalMarker(input);
+
+            marker.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+        public void FindMessageMarker_ShouldReturnExpectedMarker(string input, int expected)
+        {
+            var marker = Day06.FindMessageMarker(input);
+
+            marker.Should().Be(expected);
+        }
+    }
+}
